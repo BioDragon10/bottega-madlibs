d936ed1d384d9c07e08a9be59d37a71b32783df8
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-10 08:42:29 -0600

    placed dark skew and refactored SCSS

--- src/components/header.js
+++ src/components/header.js
@@ -3,11 +3,19 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="header">
+            <div className="header__skew"></div>
+            <div className="header__border"></div>
             <h1>Bottega Madlibs</h1>
             <p>Fill out the fields below and click the generate button
                 to see the Mad Lib story.</p>
         </div>
     )
 }
+
+//things we need to fix
+//placeholder
+// grey and green number labels
+// content labels
+// gnerate btn space/card-height
 
 export default Header;
